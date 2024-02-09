2ef6f05407fef2e5cdb7cf48704e44986a902b0c
Joao Martins <ctjoaomartins@gmail.com>
2024-02-09 03:57:10 -0300

    feat(voting): implement endpoint to fetch poll details by ID and add voting functionality to polls
    Adds a new GET endpoint '/polls/:pollId' to retrieve details of a specific poll, including its options. Leverages zod for parameter validation, ensuring the pollId is a valid UUID. Enhances the application by allowing users to view poll details dynamically. And introduces a POST endpoint '/polls/:pollId/votes' enabling users to vote on poll options. Incorporates zod for validating request body and parameters, node:crypto for generating unique session IDs, and Prisma for database interactions. Handles vote uniqueness per session, allowing vote changes while preventing multiple votes on the same poll. Implements cookie management for session tracking, improving user experience by facilitating anonymous voting with session persistence.

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,26 +1,22 @@
 
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
-import { z } from 'zod'
+import { createPoll } from './routes/create-poll'
+import { getPoll } from './routes/get-poll'
+import { voteOnPoll } from './routes/vote-on-poll'
+import cookie from '@fastify/cookie'
 
-const app = fastify();
+const app = fastify()
 
-const prisma = new PrismaClient()
-
-app.post('/polls', async (request, reply) => {
-    const createPollBody = z.object({
-        title: z.string()
-    })
-
-    const { title } =  createPollBody.parse(request.body)
-
-    const poll = await prisma.poll.create({
-        data: { title, }
-    })
-    
-    return reply.status(201).send({ pollId: poll.id })
+app.register(cookie, {
+    secret: "asASH-D98ASHDAJSKD-asKDJLsakas9-231as1a",
+    hook: 'onRequest',
 })
 
+app.register(createPoll)
+app.register(getPoll)
+app.register(voteOnPoll)
+
+
 app.listen({ port: 3333 }).then(() => {
-    console.log(`listening on port 3333` )
+    console.log(`Running on port 3333 🚀` )
 });
